02a1d9ddffaee7b7872295dc92748d5ef3b9e2c3
Juan <juan.calvo.dev@gmail.com>
2023-11-23 19:43:47 -0300

    primer commit

---
+++ src/main/java/com/nexomovilidad/nexomovilidad/NexomovilidadApplication.java
@@ -0,0 +1,13 @@
+package com.nexomovilidad.nexomovilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexomovilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexomovilidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexomovilidad/nexomovilidad/NexomovilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexomovilidad.nexomovilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexomovilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
